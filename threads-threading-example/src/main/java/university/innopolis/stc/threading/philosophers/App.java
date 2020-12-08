d558f085e8e81ef0120dc7bbd6b6a3e1985c2b1a
Anton V. Kirilchik <kosmonaffft@gmail.com>
2020-12-08 21:38:03 +0300

    Threads example...

---
+++ threads-threading-example/src/main/java/university/innopolis/stc/threading/philosophers/App.java
@@ -0,0 +1,66 @@
+package university.innopolis.stc.threading.philosophers;
+
+import com.google.common.base.Stopwatch;
+
+import java.util.Arrays;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.stream.IntStream;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    private static void process(int[] array, int start, int increment) {
+        for (int i = start; i < array.length; i += increment) {
+            array[i] = array[i] * array[i] * array[i] / 73;
+        }
+    }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        int[] ints = IntStream.range(0, 10_000_001).toArray();
+        int threadsCount = 2;
+        Thread[] threads = new Thread[threadsCount];
+        Future[] futures = new Future[threadsCount];
+        ExecutorService executorService = Executors.newFixedThreadPool(threadsCount);
+
+        Stopwatch s = Stopwatch.createStarted();
+        for (int i = 0; i < ints.length; i++) {
+            ints[i] = ints[i] * ints[i] * ints[i] / 73;
+        }
+        s.stop();
+        System.out.println(s.toString());
+
+        for (int i = 0; i < threadsCount; ++i) {
+            int start = i;
+            Thread thread = new Thread(() -> process(ints, start, threadsCount));
+            threads[i] = thread;
+        }
+
+        s = Stopwatch.createStarted();
+        Arrays.stream(threads).forEach(Thread::start);
+        Arrays.stream(threads).forEach(t -> {
+            try {
+                t.join();
+            } catch (InterruptedException ignored) {
+            }
+        });
+        s.stop();
+        System.out.println(s.toString());
+
+        s = Stopwatch.createStarted();
+        for (int i = 0; i < threadsCount; ++i) {
+            int start = i;
+            Future<?> future = executorService.submit(() -> process(ints, start, threadsCount));
+            futures[i] = future;
+        }
+        for (int i = 0; i < futures.length; i++) {
+            futures[i].get();
+        }
+        s.stop();
+        System.out.println(s.toString());
+    }
+}
