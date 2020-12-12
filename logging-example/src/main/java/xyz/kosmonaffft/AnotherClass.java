35e57e56de432a529f653dca31a7ea39b7f131a3
Anton V. Kirilchik <kosmonaffft@gmail.com>
2020-12-12 20:54:36 +0300

    Add logging example...

---
+++ logging-example/src/main/java/xyz/kosmonaffft/AnotherClass.java
@@ -0,0 +1,26 @@
+package xyz.kosmonaffft;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+
+@Data
+@Slf4j
+@EqualsAndHashCode
+@AllArgsConstructor
+public class AnotherClass {
+
+    @NonNull
+    private String field;
+
+    private String str;
+
+    public static void method() {
+        log.warn("Another error!!!");
+        log.info("Info");
+        log.debug("Ddebug");
+        log.trace("Trace");
+    }
+}
---
+++ logging-example/src/main/java/xyz/kosmonaffft/App.java
@@ -0,0 +1,29 @@
+package xyz.kosmonaffft;
+
+import lombok.Cleanup;
+import lombok.SneakyThrows;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    private final static Logger logger = LoggerFactory.getLogger(App.class);
+
+    @SneakyThrows
+    public static void main(String[] args) {
+        logger.error("Hello, world!!!");
+        logger.trace("Trace from app");
+        AnotherClass.method();
+        AnotherClass c1 = new AnotherClass("f", "c1");
+        AnotherClass c2 = new AnotherClass("f", "c2");
+        System.out.println(c1.equals(c2));
+
+        new AnotherClass("null", "");
+        System.out.println(c1.toString());
+
+        @Cleanup("getField") AnotherClass a = new AnotherClass("", "");
+    }
+}
